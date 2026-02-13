60284fae701525a65f7fac9b5acca3a3a753d5d1
Avinash Saroj <98206280+avinashsaroj01@users.noreply.github.com>
2026-02-13 09:53:03 +0530

    updated port

--- index.js
+++ index.js
@@ -151,6 +151,8 @@ app.post("/create-payment-intent", async (req, res) => {
 });
 
 /* -------------------- SERVER -------------------- */
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
